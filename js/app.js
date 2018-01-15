c7c711a39ac6b146f387d7a9d6caa8605419dec4
Ramon Mendoza <brickfazed@hotmail.com>
2018-01-15 14:58:51 -0800

    Finished building object properly and applied the method to First and Pike store and SeaTac store

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
 'use strict';
 
-var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm']
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+
+var firstAndPike = {
+  customers: [],
+  render: function() {
+    var ulEl = document.getElementById('firstAndPike');
+
+    for(var i = 0; i < hours.length; i++) {
+      this.customers.push(Math.floor(Math.random() * (65 - 23 + 1) + 23));
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ': ' + Math.round((this.customers[i] * 6.3)) + ' cookies.';
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+var seatacAirport = {
+  customers: [],
+  render: function() {
+    var ulEl = document.getElementById('seatac');
+
+    for(var i = 0; i < hours.length; i++) {
+      this.customers.push(Math.floor(Math.random() * (24 - 3 + 1) + 3));
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ': ' + Math.round((this.customers[i] * 1.2)) + ' cookies.';
+      ulEl.appendChild(liEl);
+    }
+  }
+}
+
+var seattleCenter = {
+  
+}
+
+firstAndPike.render();
+seatacAirport.render();
